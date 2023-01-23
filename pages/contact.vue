cfc3684f78ce253fc9ad0417a995bc5e593fdb98
yanuar <yanuaraditia@outlook.com>
2023-01-23 09:48:57 +0700

    New Design

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,35 +1,64 @@
 <template>
   <section class="py-16 xl:py-24">
     <div class="container px-4 xl:w-9/12 mx-auto">
-      <div class="xl:mx-auto xl:w-7/12">
-        <h1 class="font-bold mb-5 xl:mb-10 text-3xl xl:text-5xl">
-          <span>Contact Me</span>
-        </h1>
-        <div class="prose xl:text-lg dark:prose-invert xl:w-8/12">
-          <p>Inquiries via email at <a href="mailto:yanuaraditia@outlook.com">yanuaraditia@outlook.com</a> or via socials below:</p>
-          <ul>
-            <li>
-              <span>GitHub</span>
-              <span> — </span>
-              <a href="//github.com/yanuaraditia">https://github.com/yanuaraditia</a>
-            </li>
-            <li>
-              <span>GitLab</span>
-              <span> — </span>
-              <a href="//gitlab.com/ngal0r">https://github.com/ngal0r</a>
-            </li>
-            <li>
-              <span>LinkedIn</span>
-              <span> — </span>
-              <a href="//linkedin.com/in/yanuaraditia">https://linkedin.com/in/yanuaraditia</a>
-            </li>
-            <li>
-              <span>Telegram</span>
-              <span> — </span>
-              <a href="//t.me/yanuaraditia">https://t.me/yanuaraditia</a>
-            </li>
-          </ul>
-          <p>I started this website as a place to document everything I learned. I hope you enjoy my content.</p>
+      <h1 class="font-bold mb-5 text-3xl xl:text-5xl">
+        <span>Contact</span>
+      </h1>
+      <p class="text-lg mb-10 xl:w-5/12">Inquiries via email at <a href="mailto:yanuaraditia@outlook.com">yanuaraditia@outlook.com</a> or via socials below:</p>
+      <div class="grid xl:grid-cols-3 gap-3">
+        <div class="p-5 rounded-2xl group hover:ring-2 hover:ring-red-300 transition-all bg-red-100 relative">
+          <div class="inline-block p-3 bg-red-200 mb-3 rounded-full">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-github" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+               <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+               <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path>
+            </svg>
+          </div>
+          <span class="block">https://github.com/yanuaraditia</span>
+          <a href="//github.com/yanuaraditia" class="absolute top-0 left-0 right-0 bottom-0"></a>
+        </div>
+        <div class="p-5 rounded-2xl group hover:ring-2 hover:ring-red-300 transition-all bg-red-100 relative">
+          <div class="inline-block p-3 bg-red-200 mb-3 rounded-full">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-gitlab" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+               <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+               <path d="M21 14l-9 7l-9 -7l3 -11l3 7h6l3 -7z"></path>
+            </svg>
+          </div>
+          <span class="block">https://gitlab.com/ngal0r</span>
+          <a href="//gitlab.com/ngal0r" class="absolute top-0 left-0 right-0 bottom-0"></a>
+        </div>
+        <div class="p-5 rounded-2xl group hover:ring-2 hover:ring-red-300 transition-all bg-red-100 relative">
+          <div class="inline-block p-3 bg-red-200 mb-3 rounded-full">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-linkedin" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+               <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+               <rect x="4" y="4" width="16" height="16" rx="2"></rect>
+               <line x1="8" y1="11" x2="8" y2="16"></line>
+               <line x1="8" y1="8" x2="8" y2="8.01"></line>
+               <line x1="12" y1="16" x2="12" y2="11"></line>
+               <path d="M16 16v-3a2 2 0 0 0 -4 0"></path>
+            </svg>
+          </div>
+          <span class="block">https://linkedin.com/in/yanuaraditia</span>
+          <a href="https://linkedin.com/in/yanuaraditia" class="absolute top-0 left-0 right-0 bottom-0"></a>
+        </div>
+        <div class="p-5 rounded-2xl group hover:ring-2 hover:ring-red-300 transition-all bg-red-100 relative">
+          <div class="inline-block p-3 bg-red-200 mb-3 rounded-full">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-telegram" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+               <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+               <path d="M15 10l-4 4l6 6l4 -16l-18 7l4 2l2 6l3 -4"></path>
+            </svg>
+          </div>
+          <span class="block">https://t.me/yanuaraditia</span>
+          <a href="https://t.me/yanuaraditia" class="absolute top-0 left-0 right-0 bottom-0"></a>
+        </div>
+        <div class="p-5 rounded-2xl group hover:ring-2 hover:ring-red-300 transition-all bg-red-100 relative">
+          <div class="inline-block p-3 bg-red-200 mb-3 rounded-full">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-twitter" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+               <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+               <path d="M22 4.01c-1 .49 -1.98 .689 -3 .99c-1.121 -1.265 -2.783 -1.335 -4.38 -.737s-2.643 2.06 -2.62 3.737v1c-3.245 .083 -6.135 -1.395 -8 -4c0 0 -4.182 7.433 4 11c-1.872 1.247 -3.739 2.088 -6 2c3.308 1.803 6.913 2.423 10.034 1.517c3.58 -1.04 6.522 -3.723 7.651 -7.742a13.84 13.84 0 0 0 .497 -3.753c-.002 -.249 1.51 -2.772 1.818 -4.013z"></path>
+            </svg>
+          </div>
+          <span class="block">https://twitter.com/souricedcoffee</span>
+          <a href="https://twitter.com/souricedcoffee" class="absolute top-0 left-0 right-0 bottom-0"></a>
         </div>
       </div>
     </div>
