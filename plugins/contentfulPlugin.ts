a92c7250dd203d4a8adad95bb106181f20859f83
yanuar <yanuaraditia@outlook.com>
2023-02-03 09:58:13 +0700

    Update func

--- plugins/contentfulPlugin.ts
+++ plugins/contentfulPlugin.ts
@@ -1,10 +1,12 @@
+import { createClient } from "contentful"
 import contentful from 'contentful'
 
 export default defineNuxtPlugin(() => {
     const config = useRuntimeConfig()
+    const createClientFunc = process.env.NODE_ENV === 'development' ? createClient : contentful.createClient
     return {
         provide: {
-            client: contentful.createClient({
+            client: createClientFunc({
                 space: config.public.space,
                 accessToken: config.public.accessToken
             })
